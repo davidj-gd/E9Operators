3ab63a70124c291d91ac3c3b8f4aa4c7f51e507c
davidj-gd <david.johansson@student.forsbergsskola.se>
2025-09-25 21:57:43 +0200

    E9Operators
    Assignment

---
+++ Program.cs
@@ -0,0 +1,28 @@
+﻿
+Console.WriteLine("Give me a number of seconds\n");
+string answer = Console.ReadLine();
+int seconds = Convert.ToInt32(answer);
+
+int days = seconds / 86400;
+int whatsLeft = seconds % 86400;
+
+int hours = whatsLeft / 3600;
+whatsLeft %= 3600;
+
+int minutes = whatsLeft / 60;
+int sec = whatsLeft % 60;
+
+//Result
+Console.WriteLine($"Seconds: {sec}");
+Console.WriteLine($"minutes: {minutes}");
+Console.WriteLine($"hours: {hours}");
+Console.WriteLine($"days: {days}");
+
+Console.WriteLine($"{days}.{hours}:{minutes}:{sec}");
+
+double daysfraction = seconds / 86400.0;
+
+Console.WriteLine($"In total, that's {daysfraction:F16}"); /* not sure how to get
+the 7 at the end...*/
+
+
